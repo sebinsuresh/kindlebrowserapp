0f617e9a582116be395be489d2433805a1ab360a
Sebin Puthenthara Suresh <sebinsuresh@gmail.com>
2021-12-07 11:51:49 -0600

    Client: Format sensor data as object properly and send

--- client.js
+++ client.js
@@ -5,9 +5,13 @@ import fetch from "node-fetch";
 
 const port = 8081;
 const path = "sensor";
+
+const sensordata = {};
+if (process.argv.length > 3)
+  sensordata[process.argv.slice(2, -1).join(" ")] = process.argv.slice(-1)[0];
+
 fetch(`http://localhost:${port}/${path}`, {
   method: "POST",
   headers: { "Content-Type": "application/json" },
-  //   body: JSON.stringify({ key1: "val1", key2: "val2" }), // dummy data
-  body: JSON.stringify(process.argv),
+  body: JSON.stringify(sensordata),
 });
