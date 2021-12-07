4e0f3814bb85750e35d5de8fb21db35a360e2ec6
Sebin Puthenthara Suresh <sebinsuresh@gmail.com>
2021-12-06 21:32:53 -0600

    Create code to be called by sensor alerts
    - Makes a JSON POST request to the server

---
+++ client.js
@@ -0,0 +1,12 @@
+// Call this file like: node.exe client.js <arguments> and it will send
+// the sensor data to server.js and die.
+
+import fetch from "node-fetch";
+
+const port = 8081;
+const path = "sensor";
+fetch(`http://localhost:${port}/${path}`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ key1: "val1", key2: "val2" }), // dummy data
+});
