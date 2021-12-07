a4d034a969ad3b54ec570853545deb57c0d35f72
Sebin Puthenthara Suresh <sebinsuresh@gmail.com>
2021-12-07 14:13:10 -0600

    Add simple start/stop button to page

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,8 @@
 var counter = 0;
+var refreshOn = true;
 const refreshdiv = document.getElementById("refresh-content");
+const refreshbtn = document.getElementById("refresh-control-button");
+
 function addToBody() {
   var added = "hello";
   for (var i = 0; i < counter; i++) {
@@ -23,4 +26,16 @@ function testAPIReq() {
   oReq.send();
 }
 
-setInterval(testAPIReq, 2000);
+var refreshInterval = setInterval(testAPIReq, 2000);
+
+refreshbtn.addEventListener("click", function () {
+  refreshOn = !refreshOn;
+  if (!refreshOn) {
+    clearInterval(refreshInterval);
+    refreshbtn.innerText = "Start";
+    refreshdiv.innerText = "Stopped.";
+  } else {
+    refreshInterval = setInterval(testAPIReq, 2000);
+    refreshbtn.innerText = "Stop";
+  }
+});
