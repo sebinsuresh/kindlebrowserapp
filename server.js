f9bd607a2001551ef817ca8c39c141cf387676e2
Sebin Puthenthara Suresh <sebinsuresh@gmail.com>
2021-12-07 14:02:54 -0600

    Add sensordata object: Store k:v of sensors

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ import { engine } from "express-handlebars";
 // const cors = require("cors");
 const app = express();
 
+// Object to store and update the data from sensors. Keys are sensor names, and
+// values are the last values from those sensors.
+const sensordata = {};
+
 // Use hbs for templating. The name "hbs" instead of "handlebars" must be
 // used everywhere in order for it to work correctly with that extension.
 app.engine("hbs", engine({ defaultLayout: "main", extname: ".hbs" }));
@@ -20,17 +24,19 @@ app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  // TODO: Find something to polyfill for Kindle browser.
   res.render("home");
 });
 
 // Test api call
 app.get("/api", (req, res) => {
-  res.json({ randval: ~~(100 * Math.random()) });
+  res.json(sensordata);
 });
 
 app.post("/sensor", async (req, res) => {
-  console.log("Sensor data arrived!", req.body);
+  if (req.body.length == 2) {
+    sensordata[req.body[0]] = req.body[1];
+  }
+  // console.log("Currrent val of sensordata:", sensordata);
   res.sendStatus(200);
 });
 
